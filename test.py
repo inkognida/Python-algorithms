0b74bc1742894b39ddbda202c2e72b9c80280e54
Artem <53000241+inkognida@users.noreply.github.com>
2020-09-17 17:37:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -15,3 +15,22 @@ def fibb(n):
         a, b = b, a + b
 
 print(list(fibb(10)))
+
+
+def first():
+    f = input('Enter your message here: ')
+    if ('@' and '.ru') in f: print(f) #output test@gmail.ru else: Nothing
+        
+        
+def second(): 
+    import functools, string, operator
+    print('Lenght of the string:',len('На дворе - трава, на траве - дрова, не руби дрова на траве двора!'))
+    print('String with .replace' + 'На дворе - трава, на траве - дрова, не руби дрова на траве двора!'.replace('дрова', 'trash'))
+    f = string.punctuation
+    text = 'На дворе - трава, на траве - дрова, не руби дрова на траве двора!'
+    print('Punctuation symbols:',len(list(filter(functools.partial(operator.contains, f), text))))
+second() 
+''' #OUTPUT:   Lenght of the string: 65
+    String with .replaceНа дворе - трава, на траве - trash, не руби trash на траве двора!
+    Punctuation symbols: 5'''
+pass
