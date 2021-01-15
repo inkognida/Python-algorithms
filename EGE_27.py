d5f5e64fe5eaf97bb7679b624b700f4554cd1412
Artem <53000241+inkognida@users.noreply.github.com>
2021-01-15 07:56:26 +0300

    Update EGE_27.py

--- EGE_27.py
+++ EGE_27.py
@@ -2,7 +2,7 @@ with open('27-school.txt', 'r+') as f:
     pairs = [list(map(int, line.split())) for line in f][1:]
     diff = [abs(i[0] - i[1]) for i in pairs if (i[0] - i[1]) != 0 and (i[0]- i[1]) % 5 != 0]
     local_sum = sum([max(i) for i in pairs])
-if local_sum % 3 == 0: 
+if local_sum % 5 == 0: 
     local_sum -= min(diff)
     print(local_sum)
 else: 
