98f4d0a881e2abf2b493a14d75e951bf4dba6736
Artem <53000241+inkognida@users.noreply.github.com>
2021-03-03 21:13:50 +0300

    Create sys_base.py

---
+++ sys_base.py
@@ -0,0 +1,11 @@
+def sys_base(num, base): 
+    digits = '0123456789ABCDEF'
+    remain = []
+    while num > 0: 
+        remainder = num % base
+        remain.append(remainder)
+        num //= base
+    new_num = []
+    while remain: 
+        new_num.append(digits[remain.pop()])
+    return ''.join(new_num)
