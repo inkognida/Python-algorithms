0c01a40c67b1743db05a3132725526a044d565e3
Artem <53000241+inkognida@users.noreply.github.com>
2021-06-11 17:44:13 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import itertools
-chars = ['А', 'Н', 'Д', 'Р', 'Е', 'Й']
-params = list(itertools.product(chars, repeat = 6))
-count = 0 
-for par in params:
-    if (''.join(par).count('А') >= 1) and (''.join(par).count('Й') <= 1):
-        count += 1
-print(count) #24135
